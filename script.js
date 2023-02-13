2922daaaaf92470f4bcb8d8d73ecb88a52eca5a3
VladimirLeonov <vow.leonoff@yandex.ru>
2023-02-13 17:54:08 +0300

    first/movies script

---
+++ script.js
@@ -0,0 +1,44 @@
+/* 2 */
+/* const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: [numberOfFilms],
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+console.log(personalMovieDB); */
+/* 3 */
+/* const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const quantityOfFilms = prompt('Один из последних просмотренных фильмов?', '');
+const gradeOfFilms = prompt('На сколько оцените его?', '');
+const personalMovieDB = {
+    count: [numberOfFilms],
+    movies: {
+        [quantityOfFilms]: [gradeOfFilms],
+    },
+    actors: {},
+    genres: [],
+    privet: false,
+};
+console.log([personalMovieDB.movies]); */
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
