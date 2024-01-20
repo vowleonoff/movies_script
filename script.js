b12cb4ef3ea353da0a818dad076001b0fe9de1f7
Vladimir <vow.leonoff@yandex.ru>
2024-01-20 15:03:14 +0300

    change movie script

--- script.js
+++ script.js
@@ -1,53 +1,19 @@
-/* 2 */
-/* const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-    count: [numberOfFilms],
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false,
+    privat: false
 };
-console.log(personalMovieDB); */
-/* 3 */
-/* const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-const quantityOfFilms = prompt('Один из последних просмотренных фильмов?', '');
-const gradeOfFilms = prompt('На сколько оцените его?', '');
-const personalMovieDB = {
-    count: [numberOfFilms],
-    movies: {
-        [quantityOfFilms]: [gradeOfFilms],
-    },
-    actors: {},
-    genres: [],
-    privet: false,
-};
-console.log([personalMovieDB.movies]); */
 
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = +prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
-
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false,
-// };
-
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
-
-
-//
-
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(personalMovieDB);
